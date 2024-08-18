d5d129b37a50fa9bd27b0a22506f625b2977b513
orgamiryazdani <orgamiryazdani@gmail.com>
2024-08-18 22:54:49 +0330

    Fix Persian styles and styles related to carousel

--- src/components/products/ProductDetails.tsx
+++ src/components/products/ProductDetails.tsx
@@ -9,6 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../ui/carousel";
+import { isPersian } from "../../utils/isPersian";
 
 const ProductDetails = () => {
   const [searchParams] = useSearchParams();
@@ -19,9 +20,11 @@ const ProductDetails = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <div className='w-full h-auto flex items-center justify-start flex-col' dir='ltr'>
+    <div className='w-full h-auto flex items-center justify-start flex-col'>
       {/* Carousel */}
-      <Carousel className='w-auto relative' dir='ltr'>
+      <Carousel
+        className='relative w-full'
+        dir='ltr'>
         <CarouselContent>
           {data?.images.map((image, index) => (
             <CarouselItem key={index}>
@@ -43,17 +46,21 @@ const ProductDetails = () => {
         </div>
       </Carousel>
       {/* info */}
-      <div>
+      <div dir={isPersian(data?.title || "") ? "rtl" : "ltr"} className="w-full">
         <p className='text-xl font-bold mt-3'>{data?.title}</p>
         <p className='text-sm my-2'>{data?.description}</p>
-        <div className='full flex items-end justify-between mt-4'>
+        <div
+          className='full flex items-end justify-between mt-4'
+          dir='ltr'>
           <div className='w-auto bg-accent h-auto rounded-md flex items-center'>
             <img
               className='w-10 h-10 rounded-md'
               src={data?.category.image}
               alt={data?.category.name}
             />
-              <p className='px-2 text-sm text-muted-foreground'># {data?.category.name}</p>
+            <p className='px-2 text-sm text-muted-foreground'>
+              # {data?.category.name}
+            </p>
           </div>
           <p className='text-green-500 text-xl'>$ {data?.price}</p>
         </div>
